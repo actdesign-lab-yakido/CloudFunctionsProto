5b4c6b6e37d250e068d8314892a77a7fb676dc97
yakido <yakido@actdesign-lab.co.jp>
2021-05-27 17:52:45 +0900

    change indent

--- dc2_proto_web/index.js
+++ dc2_proto_web/index.js
@@ -1,27 +1,29 @@
+process.env.X_GOOGLE_NEW_FUNCTION_SIGNATURE = true;// 環境変数取得用設定
 const express = require('express');
 const app = express();
 const axiosBase = require("axios");
+const baseURL = 'https://' + process.env.REGION + '-' + process.env.PROJECT + '.cloudfunctions.net';
 const axios = axiosBase.create({
-  baseURL: 'https://asia-northeast1-itengineer.cloudfunctions.net/',
-  timeout: 3000,
-  headers: {
-    'Content-Type': 'application/json',
-    'X-Requested-With': 'XMLHttpRequest'
-  },
-  responseType: 'json'  
+	baseURL: baseURL,
+	timeout: 3000,
+	headers: {
+		'Content-Type': 'application/json',
+		'X-Requested-With': 'XMLHttpRequest'
+	},
+	responseType: 'json'
 });
 
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-  axios.get('/dc2_proto_api/users')
-  .then(function(response) {
-	res.render('pages/index', {users:response.data} );
-  })
-  .catch(function(error) {
-	console.log('ERROR!! occurred in api.')
-  });
+	axios.get('/dc2_proto_api/users')
+	.then(function(response) {
+		res.render('pages/index', {users:response.data} );
+	})
+	.catch(function(error) {
+		console.log('ERROR!! occurred in api.')
+	});
 });
 
 module.exports = {
-  app
+	app
 };
